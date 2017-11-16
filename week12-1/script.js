810dac93a9144848f5c81c770ac2dae01842035b
jmatiz <jay.tracker@gmail.com>
2017-11-16 18:07:37 -0500

    Add files via upload

---
+++ week12-1/script.js
@@ -0,0 +1,7 @@
+function changeImage() {
+    document.getElementById('image').src='http://static2.businessinsider.com/image/5970c122c50c298d088b5692/everything-to-know-about-the-unexpected-and-dramatic-firing-of-kermit-the-frog-actor-steve-whitmire-after-27-years.jpg';
+}
+
+function changebackImage() {
+    document.getElementById('image').src='https://pbs.twimg.com/profile_images/839900475205955585/FMzXSOkV.jpg';
+}
